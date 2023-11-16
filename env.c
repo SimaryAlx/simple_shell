051e63e189a25a475633f4d4576f02ffe59bb40b
lSimaryl <70992381+SimaryAlx@users.noreply.github.com>
2023-11-16 03:06:28 +0200

    env.c

--- env.c
+++ env.c
@@ -2,7 +2,7 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain crop file still
  *          constant function prototype.
  * Return: Always 0
  */
